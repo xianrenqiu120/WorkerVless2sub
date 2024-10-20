e1590a2cc290cd25873e3ace5e841090ced62915
xianrenqiu120 <0gUFVzWYheUultZb@proton.me>
2024-10-20 22:46:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,10 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
 	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	'cloudflare.cfgo.cc#优选官方线路',  
+	'www.visa.com.sg#官方优选域名',
+	'www.wto.org:8080#官方优选域名',
+	'www.who.int:8880#官方优选域名',
 ];
 
 // 设置优选地址api接口
