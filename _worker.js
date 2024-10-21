c5a601f76cdf4cfe7a034b6d095c60db22a8112c
xianrenqiu120 <0gUFVzWYheUultZb@proton.me>
2024-10-21 12:04:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,7 +12,8 @@ let addresses = [
 // 设置优选地址api接口
 let addressesapi = [
 	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt?proxyip=true', 
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', 
+	'https://raw.githubusercontent.com/xianrenqiu120/zijian/refs/heads/main/zijian.txt'
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
